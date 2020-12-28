048aa807425e093cecc2a8bb258d5bb9cb1c76e7
scor <kimmo.kuohula@gmail.com>
2020-12-29 00:48:44 +0200

    small update on printcostumedataseeds

--- front/src/comp/admin/prints/data/PrintCostumeDataSeeds.js
+++ front/src/comp/admin/prints/data/PrintCostumeDataSeeds.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { GetDataHook } from '../../../hooks/SeedHooks';
 
-const SingleCostume = ({costume}) => {
+const SingleCostume = ({costume, index}) => {
+    const idnum = index+1;
     return (
     <div>
-        &#123;&nbsp;cid&#58;&nbsp;{costume.cid}&#44;&nbsp;cpid&#58;&nbsp;{costume.cpid}&#44;&nbsp;version&#58;&nbsp;&#39;{costume.version}&#39;&#44;&nbsp;cfirstappearance&#58;&nbsp;{costume.cfirstappearance !== null ? "'" + costume.cfirstappearance + "'" : "null"}&#44;&nbsp;pcupdated&#58;&nbsp;{costume.pcupdated !== null ? costume.pcupdated : "null"}&#44;&nbsp;costumeimg&#58;&nbsp;&#39;{costume.costumeimg}&#39;&#125;&#44;
+        &#123;&nbsp;cid&#58;&nbsp;{idnum}&#44;&nbsp;cpid&#58;&nbsp;{costume.cpid}&#44;&nbsp;version&#58;&nbsp;&#39;{costume.version}&#39;&#44;&nbsp;cfirstappearance&#58;&nbsp;{costume.cfirstappearance !== null ? "'" + costume.cfirstappearance + "'" : "null"}&#44;&nbsp;pcupdated&#58;&nbsp;{costume.pcupdated !== null ? "'" + costume.pcupdated + "'" : "null"}&#44;&nbsp;costumeimg&#58;&nbsp;&#39;{costume.costumeimg}&#39;&#125;&#44;
     </div>
     );
 };
@@ -19,7 +20,7 @@ const PrintCostumeDataSeeds = () => {
         </div>
         <div className="container pad1em">
             module&#46;exports &#61; &#91;
-            {costumeData.map(c => <SingleCostume key={c.aid} id={c.aid} costume={c} />)}
+            {costumeData.map((c, index) => <SingleCostume key={c.aid} id={c.aid} costume={c} index={index} />)}
             &#93;
         </div>
         </>
